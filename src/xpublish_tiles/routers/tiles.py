667bc5faefa7f5d1cc92c1e5d0f3a935b901ac99
Matthew Iannucci <mpiannucci@gmail.com>
2025-07-23 16:06:24 -0400

    Cleanup old routers

--- src/xpublish_tiles/routers/tiles.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-
-tiles_router = APIRouter()
-
-
-@tiles_router.get("/")
-async def xyz_tile():
-    # TODO: Implement XYZ Tiles
-    return {"message": "Hello World from tiles router"}
