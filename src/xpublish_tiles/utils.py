1aa693c678faaeaee94624a6ff64e8c16f3f8e1f
Matthew Iannucci <mpiannucci@gmail.com>
2025-08-25 12:23:05 -0400

    refine logging

--- src/xpublish_tiles/utils.py
+++ src/xpublish_tiles/utils.py
@@ -20,7 +20,7 @@ def timeit(func):
         start_time = time.time()
         result = func(*args, **kwargs)
         end_time = time.time()
-        logger.debug(f"{func.__name__} took {end_time - start_time} seconds")
+        logger.debug(f"{func.__name__}: {end_time - start_time} ms")
         return result
 
     return wrapper
